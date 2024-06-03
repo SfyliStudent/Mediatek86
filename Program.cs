78fe78b63c229112a1e30a889b195547fc71cd3c
SfyliStudent <safiyalarabi@gmail.com>
2024-06-03 12:39:00 +0200

    Mission 0 : initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using MediaTekDocuments.view;
+using System;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace MediaTekDocuments
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmMediatek());
+
+           
+
+          
+            
+            // Pour éviter que la console ne se ferme immédiatement
+            Console.WriteLine("Appuyez sur une touche pour continuer...");
+            Console.ReadKey();
+        }
+    }
+}
